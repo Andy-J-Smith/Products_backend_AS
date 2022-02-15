5c3487e433588b0792622559f7fccf90342e41c3
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-02-15 13:44:06 -0600

    Created products views + created products admin superuser

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
+from tokenize import Pointfloat
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+admin.site.register(Product)
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view
-from django.shortcuts import get_list_or_404, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
