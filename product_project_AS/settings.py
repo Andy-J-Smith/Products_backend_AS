4672243a548546a00fadb462d28a747c92c709c0
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-02-15 11:08:36 -0600

    Made db connections

--- product_project_AS/settings.py
+++ product_project_AS/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-01_l965e8%!u%%rp%&n^bnf+$d8u&r%s8ydb#63iboj@xm=#7j'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -116,3 +115,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from product_project_AS.local_settings import *
+except ImportError:
+    pass
